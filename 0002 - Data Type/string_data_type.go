33c6f88d477c916f07128d87d529fa835eb92823
Iqbal_Arrafi <iqbalarrafi39@gmail.com>
2023-05-31 15:03:05 +0700

    string data type

---
+++ 0002 - Data Type/string_data_type.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Iqbal"
+	fullName := "Iqbal Arrafi"
+
+	fmt.Println(name)
+	fmt.Println(fullName)
+
+	//return total length of the character on string
+	fmt.Println(len(name))
+
+	//return byte value of the character on string
+	fmt.Println(fullName[0])
+}

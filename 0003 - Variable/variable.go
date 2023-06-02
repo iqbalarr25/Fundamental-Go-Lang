6559af6e64edd8e88d6ca93f30ff2b00c73dc087
Iqbal_Arrafi <iqbalarrafi39@gmail.com>
2023-06-02 10:39:20 +0700

    variable writing

---
+++ 0003 - Variable/variable.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Iqbal Arrafi"
+	fmt.Println(name)
+
+	var friendName = "Waduh"
+	fmt.Println(friendName)
+
+	enemyName := "David"
+	fmt.Println(enemyName)
+
+	var (
+		firstName = "Iqbal"
+		lastName  = "Arrafi"
+		age       = 20
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(age)
+}
